8b4da3dad5888591636963c035ca741e4af3d41e
bluelove8939 <bluelove8939@naver.com>
2022-08-19 18:43:13 +0900

    Initial commit

--- tb_python_compression_result_analyzer.py
+++ tb_python_compression_result_analyzer.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 parser = argparse.ArgumentParser(description='Test Result Analyzing Configs')
-parser.add_argument('-fp', '--filepath', default=os.path.join(os.curdir, 'logs', 'compression_test_result_quant_int8_64B_zlib_inc.csv'),
+parser.add_argument('-fp', '--filepath', default=os.path.join(os.curdir, 'logs', 'compression_test_result_quant_int8_64B_ebdi_inc.csv'),
                     help='Path to result csv file', dest='filepath')
 comp_args, _ = parser.parse_known_args()
 
