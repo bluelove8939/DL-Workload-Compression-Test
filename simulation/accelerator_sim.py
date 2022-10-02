4662b6442871ddb6e25d3b07eaa78258ca73661f
bluelove8939 <bluelove8939@naver.com>
2022-10-02 22:03:45 +0900

    Initial commit

--- simulation/accelerator_sim.py
+++ simulation/accelerator_sim.py
@@ -1,4 +1,4 @@
-from typing import Callable, Dict
+from typing import Callable, Dict, List
 import torch
 import numpy as np
 from scipy.sparse import csr_matrix
@@ -7,6 +7,98 @@ from compression.binary_array import array2binary
 from compression.algorithms import bdizv_compression, bdi1b_compression, csr_compression, csc_compression, zeroval_compression
 
 from models.tools.lowering import weight_lowering, ifm_lowering, ConvLayerInfo
+
+
+class AcceleratorConfig(object):
+    def __init__(self, ve_num: int=128, mac_cycle: int=1,
+                 sparse_aware: bool=True, scheduler: bool=False):
+
+        self.ve_num = ve_num        # number of VEs
+        self.mac_cycle = mac_cycle  # mac cycle
+
+        self.sparse_aware = sparse_aware
+        self.scheduler = scheduler
+
+
+class CycleSim(object):
+    def __init__(self, ac_config: AcceleratorConfig, quant: bool=True, device: str='cpu'):
+        self.ac_config = ac_config  # accelerator configuration
+        self.cycle_result = {}      # cycle result
+
+        self.quant = quant    # indicating whether target model is quantized (default: True)
+        self.device = device  # pytorch device
+
+        self.ve_queue: np.ndarray or None = None
+
+    def get_cycle(self):
+        return self.cycle_result
+
+    def _sim_init(self):
+        self.ve_queue = np.array([0] * self.ac_config.ve_num)
+
+    def register_model(self, model: torch.nn.Module, model_name: str='default'):
+        model_name = type(model).__name__ if model_name == 'default' else model_name
+        layer_info = ConvLayerInfo.generate_from_model(model, input_shape=(1, 3, 226, 226), device=self.device)
+
+        def generate_hook(model_name: str, layer_name : str) -> Callable:
+            def cycle_sim_check_hook(layer : torch.nn.Module, input_tensor : torch.Tensor, output_tensor : torch.Tensor):
+                # Generation of lowered input feature map and weight data
+                print(f"Generating lowered data with layer: {layer_name}", end='')
+
+                result_key = (model_name, layer_name)
+
+                if 'conv' not in type(layer).__name__.lower():
+                    pass
+
+                ifm = input_tensor[0]
+                weight = model.state_dict()[layer_name + '.weight']
+
+                if self.quant:
+                    ifm = ifm.int_repr()
+                    weight = weight.int_repr()
+
+                lowered_ifm = ifm_lowering(ifm=ifm, layer_info=layer_info[layer_name]).detach().cpu().numpy()
+                lowered_weight = weight_lowering(weight=weight,
+                                                 layer_info=layer_info[layer_name]).detach().cpu().numpy()
+
+                iw, ivecw = lowered_ifm.shape
+                ww, wvecw = lowered_weight.shape
+
+                if ivecw != wvecw:
+                    raise Exception(f'Lowering algorithm may have an error {lowered_ifm.shape} {lowered_weight.shape}')
+
+                # Run cycle test
+                print(f"\rRun cycle test: {layer_name}", end='')
+
+                self._sim_init()  # initialize VE queue
+                op_cycle = 0
+
+                imask = lowered_ifm != 0
+                wmask = lowered_weight != 0
+
+                for ipivot in range(0, iw, self.ac_config.ve_num):
+                    for w_idx in range(0, ww, 1):
+                        for ve_idx in range(self.ac_config.ve_num if (iw - ipivot >= self.ac_config.ve_num) else (iw - ipivot)):
+                            im_vec = imask[ipivot + ve_idx]
+                            wm_vec = wmask[w_idx]
+                            self.ve_queue[ve_idx] += np.count_nonzero(np.logical_and(im_vec, wm_vec))
+
+                        op_cycle += np.min(self.ve_queue)
+                        self.ve_queue -= np.min(self.ve_queue)
+
+                sparse_cycle = op_cycle + np.max(self.ve_queue)
+                dense_cycle = ((iw // self.ac_config.ve_num) + (1 if iw % self.ac_config.ve_num else 0)) * ww * ivecw
+                self.cycle_result[result_key] = (sparse_cycle, dense_cycle)
+
+                print(f"\rSimulation finished with layer: {layer_name}    "
+                      f"sparse: {sparse_cycle}    "
+                      f"dense: {dense_cycle}", end='\n')
+
+            return cycle_sim_check_hook
+
+        for layer_name, sublayer in model.named_modules():
+            if 'conv' in type(sublayer).__name__.lower() and hasattr(sublayer, 'weight'):
+                sublayer.register_forward_hook(generate_hook(model_name, layer_name))
 
 
 class AcceleratorSim(object):
