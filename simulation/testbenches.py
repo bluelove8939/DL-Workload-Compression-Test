896862c254b5d7fb8c4834d0e5b913f4c08393ce
bluelove8939 <bluelove8939@naver.com>
2023-01-05 17:51:32 +0900

    Initial commit

--- simulation/testbenches.py
+++ simulation/testbenches.py
@@ -1,19 +1,19 @@
 testbenches = {
-    # # VGG16
-    # ("VGG16", "features.2"):  'VC1',
-    # ("VGG16", "features.7"):  'VC2',
-    # ("VGG16", "features.12"): 'VC3',
+    # VGG16
+    ("VGG16", "features.2"):  'VC1',
+    ("VGG16", "features.7"):  'VC2',
+    ("VGG16", "features.12"): 'VC3',
     ("VGG16", "features.19"): 'VC4',
 
-    # # ResNet50
-    # ("ResNet50", "layer1.0.conv2"): 'RC1',
-    # ("ResNet50", "layer2.3.conv2"): 'RC2',
-    # ("ResNet50", "layer3.5.conv2"): 'RC3',
-    # ("ResNet50", "layer4.2.conv2"): 'RC4',
+    # ResNet50
+    ("ResNet50", "layer1.0.conv2"): 'RC1',
+    ("ResNet50", "layer2.3.conv2"): 'RC2',
+    ("ResNet50", "layer3.5.conv2"): 'RC3',
+    ("ResNet50", "layer4.2.conv2"): 'RC4',
 
-    # # AlexNet
-    # ("AlexNet", "features.3"):  'AC1',
-    # ("AlexNet", "features.6"):  'AC2',
+    # AlexNet
+    ("AlexNet", "features.3"):  'AC1',
+    ("AlexNet", "features.6"):  'AC2',
     ("AlexNet", "features.10"): 'AC3',
 }
 
